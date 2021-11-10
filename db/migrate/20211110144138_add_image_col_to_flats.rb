e8399e582e10207f6d4e3097c56899c220100d07
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-10 17:03:28 +0100

    my simple airbnb app

---
+++ db/migrate/20211110144138_add_image_col_to_flats.rb
@@ -0,0 +1,5 @@
+class AddImageColToFlats < ActiveRecord::Migration[6.0]
+  def change
+    add_column :flats, :picture, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'start seeding'
+desc = ["cool place", "well designed", "with terasse", "with balcony"]
+img = ["https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80","https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80","https://source.unsplash.com/random/400x200","https://images.unsplash.com/photo-1445019980597-93fa8acb246c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1474&q=80", "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=898&q=80", "https://images.unsplash.com/photo-1519449556851-5720b33024e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80", "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"]
+Flat.destroy_all
+
+
+3.times do
+  flat  = Flat.new(
+    name: Faker::Name.unique.name,
+    address: Faker::Address.city,
+    description: desc.sample,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(2..6),
+    picture: img.sample
+  )
+  flat.save!
+end
+
+puts 'seeding end...'
